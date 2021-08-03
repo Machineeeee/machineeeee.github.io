a98abe63686ffa2929e64cf65c8a970dcacf41c9
maxing <826102874@qq.com>
2021-08-03 10:48:44 +0800

    8.3.1

--- miao/lodash/machineeeee-lodash.js
+++ miao/lodash/machineeeee-lodash.js
@@ -39,6 +39,92 @@ var machineeeee = function () {
     else
       return true;
   }
+
+  function isInteger(value) {
+    return isFinite(value) && parseInt(value) == value;
+  }
+
+  function isError(value) {
+    return typeof value == "object" && value.__proto__ == Error.prototype;
+  }
+
+  function isFinite(value) {
+    return !isNaN(value) && value != Infinity && value != -Infinity;
+  }
+
+  function isFunction(value) {
+    return value.__proto__ === Function.prototype;
+  }
+
+  function isLength(value) {
+    return isInteger(value) && value >= 0;
+  }
+
+  function isMap(value) {
+    return value.__proto__ === Map.prototype;
+  }
+
+  function isNaN(value) {
+    if (typeof value === "object") {
+      value = value.valueof();
+    }
+    return value !== value;
+  }
+
+  function isNil(value) {
+    return value == undefined;
+  }
+
+  function isNull(value) {
+    return value === null;
+  }
+
+  function isNumber(value) {
+    return value.__proto__ === Number.prototype;
+  }
+
+  function isObject(value) {
+    return value != null && (typeof value === "object" || typeof value === "function");
+  }
+
+  function isObjectLike(value) {
+    return value != null && typeof value === "object";
+  }
+
+  function isPlainObject(value) {
+    return typeof value === "object" && (value.__proto__ === Object.prototype || value.__proto__ === null)
+  }
+
+  function isSafeInteger(value) {
+    return isInteger(value) && value <= Number.MAX_SAFE_INTEGER && value >= Number.MIN_SAFE_INTEGER;
+  }
+
+  function isSet(value) {
+    return value.__proto__ === Set.prototype;
+  }
+
+  function isString(value) {
+    return typeof value === "string" || (typeof value === "object" && value.__proto__ === String.prototype);
+  }
+  function isSymbol(value) {
+    return Object.prototype.toString.call(value) === "[object Symbol]";
+  }
+
+  function isTypedArray(value) {
+    return Object.prototype.toString.call(value) === "[object Uint8Array]";
+  }
+
+  function isUndefined(value) {
+    return Object.prototype.toString.call(value) === "[object Undefined]";
+  }
+
+  function isWeakMap(value) {
+    return Object.prototype.toString.call(value) === "[object WeakMap]";
+  }
+
+  function isWeakSet(value) {
+    return Object.prototype.toString.call(value) === "[object WeakSet]";
+  }
   return {
     isArguments: isArguments,
     isArray: isArray,
@@ -48,6 +134,26 @@ var machineeeee = function () {
     isBuffer: isBuffer,
     isDate: isDate,
     isEmpty: isEmpty,
-
+    isError: isError,
+    isFinite: isFinite,
+    isFunction: isFunction,
+    isInteger: isInteger,
+    isLength: isLength,
+    isMap: isMap,
+    isNaN: isNaN,
+    isNil: isNil,
+    isNull: isNull,
+    isNumber: isNumber,
+    isObject: isObject,
+    isObjectLike: isObjectLike,
+    isPlainObject: isPlainObject,
+    isSafeInteger: isSafeInteger,
+    isSet: isSet,
+    isString: isString,
+    isSymbol: isSymbol,
+    isTypedArray: isTypedArray,
+    isUndefined: isUndefined,
+    isWeakMap: isWeakMap,
+    isWeakSet: isWeakSet,
   }
 }();
